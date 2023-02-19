074c72fe336a1c46d47bb3a1311dd49d7fd17c7f
Louis Wheeler <louiswheeleriv@gmail.com>
2023-02-19 13:17:30 -0500

    Convert from vanilla graphql server to apollo server

--- src/graphql/resolvers.ts
+++ src/graphql/resolvers.ts
@@ -15,40 +15,63 @@ class Message {
 // For now create a mock database keyed on object IDs
 var fakeDatabase = {};
 
-// Define resolver functions for each GraphQL API operation
 export default {
-  getMessage: (args: { id: string }): Message => {
-    if (!fakeDatabase[args.id]) {
-      throw new Error('no message exists with id ' + args.id);
-    }
-    return fakeDatabase[args.id];
+  Query: {
+    getMessage: (
+      parent,
+      args: { id: string },
+      contextValue,
+      info
+    ): Message => {
+      if (!fakeDatabase[args.id]) {
+        throw new Error('no message exists with id ' + args.id);
+      }
+      return fakeDatabase[args.id];
+    },
+    quoteOfTheDay: (): string => {
+      return Math.random() < 0.5 ? 'Take it easy' : 'Salvation lies within';
+    },
+    random: (): number => {
+      return Math.random();
+    },
+    rollDice: (
+      parent,
+      args: { numDice: number, numSides: number },
+      contextValue,
+      info
+    ): number[] => {
+      var output: number[] = [];
+      for (var i = 0; i < args.numDice; i++) {
+        output.push(1 + Math.floor(Math.random() * (args.numSides || 6)));
+      }
+      return output;
+    },
   },
-  createMessage: (args: { input: { content: string, author: string }}): Message => {
-    // Create a random id for our "database".
-    var id = randomBytes(10).toString('hex');
-    var msg = new Message(id, args.input.content, args.input.author);
-    fakeDatabase[id] = msg;
-    return msg;
-  },
-  updateMessage: (args: { id: string, input: { content: string, author: string }}): Message => {
-    if (!fakeDatabase[args.id]) {
-      throw new Error('no message exists with id ' + args.id);
-    }
-    var msg = new Message(args.id, args.input.content, args.input.author);
-    fakeDatabase[args.id] = msg;
-    return msg;
-  },
-  quoteOfTheDay: (): string => {
-    return Math.random() < 0.5 ? 'Take it easy' : 'Salvation lies within';
-  },
-  random: (): number => {
-    return Math.random();
-  },
-  rollDice: (args: { numDice: number, numSides: number }): number[] => {
-    var output: number[] = [];
-    for (var i = 0; i < args.numDice; i++) {
-      output.push(1 + Math.floor(Math.random() * (args.numSides || 6)));
-    }
-    return output;
+  Mutation: {
+    createMessage: (
+      parent,
+      args: { input: { content: string, author: string }},
+      contextValue,
+      info
+    ): Message => {
+      // Create a random id for our "database".
+      var id = randomBytes(10).toString('hex');
+      var msg = new Message(id, args.input.content, args.input.author);
+      fakeDatabase[id] = msg;
+      return msg;
+    },
+    updateMessage: (
+      parent,
+      args: { id: string, input: { content: string, author: string }},
+      contextValue,
+      info
+    ): Message => {
+      if (!fakeDatabase[args.id]) {
+        throw new Error('no message exists with id ' + args.id);
+      }
+      var msg = new Message(args.id, args.input.content, args.input.author);
+      fakeDatabase[args.id] = msg;
+      return msg;
+    },
   }
 };
