5bee6880727d7e617a31583bc91a0156cd0052da
Louis Wheeler <louiswheeleriv@gmail.com>
2023-02-19 14:37:22 -0500

    Autogenerate graphql types

--- src/graphql/schema.ts
+++ src/graphql/schema.ts
@@ -1,27 +1,5 @@
-import { buildSchema } from 'graphql';
+import { readFileSync } from 'fs';
 
-// Construct a schema, using GraphQL schema language
-export default buildSchema(`
-  input MessageInput {
-    content: String
-    author: String
-  }
+const schemaPath = './src/graphql/schema.graphql';
 
-  type Message {
-    id: ID!
-    content: String
-    author: String
-  }
-
-  type Mutation {
-    createMessage(input: MessageInput): Message
-    updateMessage(id: ID!, input: MessageInput): Message
-  }
-
-  type Query {
-    getMessage(id: ID!): Message
-    quoteOfTheDay: String
-    random: Float!
-    rollDice(numDice: Int!, numSides: Int): [Int]
-  }
-`);
+export default readFileSync(schemaPath, { encoding: 'utf-8' });
