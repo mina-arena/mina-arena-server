dc0ab7bddf0d87cc9283f940a4464670fbf875e7
Coby <contact@45930.xyz>
2023-02-26 13:42:42 -0500

    wrapping in transaction

--- src/db/seeders/20230226163601-demo-data.js
+++ src/db/seeders/20230226163601-demo-data.js
@@ -3,40 +3,53 @@
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up(queryInterface, Sequelize) {
-    await queryInterface.bulkInsert('Players', [{
-      name: 'Player 1',
-      minaPublicKey: 'B62qpYmDbDJAyADVkJzydoz7QeZy1ZTiWeH1LSuyMxXezvu5mAQi53U',
-      createdAt: new Date(),
-      updatedAt: new Date()
-    },
-    {
-      name: 'Player 2',
-      minaPublicKey: 'B62qpge4uMq4Vv5Rvc8Gw9qSquUYd6xoW1pz7HQkMSHm6h1o7pvLPAN',
-      createdAt: new Date(),
-      updatedAt: new Date()
-    }]);
+    const transaction = await queryInterface.sequelize.transaction();
+    try {
+      await queryInterface.bulkInsert('Players', [{
+        name: 'Player 1',
+        minaPublicKey: 'B62qpYmDbDJAyADVkJzydoz7QeZy1ZTiWeH1LSuyMxXezvu5mAQi53U',
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },
+      {
+        name: 'Player 2',
+        minaPublicKey: 'B62qpge4uMq4Vv5Rvc8Gw9qSquUYd6xoW1pz7HQkMSHm6h1o7pvLPAN',
+        createdAt: new Date(),
+        updatedAt: new Date()
+      }]);
 
-    await queryInterface.bulkInsert('Units', [{
-      name: 'Spearman',
-      attackPower: 3,
-      armor: 1,
-      maxHealth: 4,
-      movementSpeed: 1,
-      createdAt: new Date(),
-      updatedAt: new Date()
-    }]);
+      await queryInterface.bulkInsert('Units', [{
+        name: 'Spearman',
+        attackPower: 3,
+        armor: 1,
+        maxHealth: 4,
+        movementSpeed: 1,
+        createdAt: new Date(),
+        updatedAt: new Date()
+      }]);
 
-    await queryInterface.bulkInsert('Games', [{
-      status: 'pending',
-      createdAt: new Date(),
-      updatedAt: new Date()
-    }]);
-
+      await queryInterface.bulkInsert('Games', [{
+        status: 'pending',
+        createdAt: new Date(),
+        updatedAt: new Date()
+      }]);
+      await transaction.commit();
+    } catch (err) {
+      await transaction.rollback();
+      throw err;
+    }
   },
 
   async down(queryInterface, Sequelize) {
-    await queryInterface.bulkDelete('Players', null, {});
-    await queryInterface.bulkDelete('Units', null, {});
-    await queryInterface.bulkDelete('Games', null, {});
+    const transaction = await queryInterface.sequelize.transaction();
+    try {
+      await queryInterface.bulkDelete('Players', null, {});
+      await queryInterface.bulkDelete('Units', null, {});
+      await queryInterface.bulkDelete('Games', null, {});
+      await transaction.commit();
+    } catch (err) {
+      await transaction.rollback();
+      throw err;
+    }
   }
 };
