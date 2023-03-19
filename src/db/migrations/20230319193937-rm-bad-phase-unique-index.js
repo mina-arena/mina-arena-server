f693b39cc11a6c51145726205a144686db82f3d3
Louis Wheeler <louiswheeleriv@gmail.com>
2023-03-19 16:17:25 -0400

    Migrations to fix unique index on GamePhase

---
+++ src/db/migrations/20230319193937-rm-bad-phase-unique-index.js
@@ -0,0 +1,38 @@
+'use strict';
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    const transaction = await queryInterface.sequelize.transaction();
+    try {
+      await queryInterface.removeIndex(
+        'GamePhases',
+        'unique_game_phase_game_turn_phase',
+        { transaction }
+      );
+      await transaction.commit();
+    } catch (err) {
+      await transaction.rollback();
+      throw err;
+    }
+  },
+
+  async down (queryInterface, Sequelize) {
+    const transaction = await queryInterface.sequelize.transaction();
+    try {
+      await queryInterface.addIndex(
+        'GamePhases',
+        {
+          fields: ['gameId', 'turnNumber', 'phase'],
+          unique: true,
+          name: 'unique_game_phase_game_turn_phase'
+        },
+        { transaction }
+      );
+      await transaction.commit();
+    } catch (err) {
+      await transaction.rollback();
+      throw err;
+    }
+  }
+};
