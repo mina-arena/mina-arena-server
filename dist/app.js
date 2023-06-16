3c0e5976465bbb349231effedf3af8ae7be346d6
Coby <contact@45930.xyz>
2023-06-15 20:31:41 -0400

    adding newrelic (#40)

--- dist/app.js
+++ dist/app.js
@@ -2,6 +2,7 @@ import express from 'express';
 import { ApolloServer } from '@apollo/server';
 import { expressMiddleware } from '@apollo/server/express4';
 import { ApolloServerPluginDrainHttpServer } from '@apollo/server/plugin/drainHttpServer';
+import createNewRelicPlugin from '@newrelic/apollo-server-plugin';
 import http from 'http';
 import cors from 'cors';
 import bodyParser from 'body-parser';
@@ -13,10 +14,11 @@ const app = express();
 const webPort = process.env.PORT || 3000;
 const websocketPort = 443;
 const httpServer = http.createServer(app);
+const newRelicPlugin = createNewRelicPlugin({});
 const apolloServer = new ApolloServer({
     typeDefs,
     resolvers,
-    plugins: [ApolloServerPluginDrainHttpServer({ httpServer })],
+    plugins: [ApolloServerPluginDrainHttpServer({ httpServer }), newRelicPlugin],
 });
 await apolloServer.start();
 app.use(cors(), bodyParser.json(), expressMiddleware(apolloServer));
