18a715d35fd22890e190a5408425e4b76ab3390c
Coby <contact@45930.xyz>
2023-07-30 14:51:48 -0400

    rm comment about warning on error

--- src/service_objects/game_piece_action_resolvers/melee_attack_resolver.ts
+++ src/service_objects/game_piece_action_resolvers/melee_attack_resolver.ts
@@ -165,7 +165,6 @@ export default async function resolveMeleeAttackAction(
   const totalDamageAverage = resolvedAttack.averageDamage;
 
   // Attempt to apply the move action to the game state
-  // Warn on console for failure
   const rngPublicKey = PublicKey.fromBase58(process.env.RNG_PUBLIC_KEY);
   try {
     const serverPrivateKey = PrivateKey.fromBase58(
